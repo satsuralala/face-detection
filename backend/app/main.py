535d98ceaa85500515120f8c452861d563bfbe58
satsuralala <satsuraltumurbat@gmail.com>
2025-09-02 18:31:18 +0800

    add

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.core.database import connect_to_mongo
 from app.routes.info import router as info_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # or "*"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(title="My FastAPI + MongoDB Project")
 
